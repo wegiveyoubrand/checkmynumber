b122a08d89ee9a346c6d5f86413413c4743b1f16
wegiveyoubrand <wegiveyoubrand@gmail.com>
2021-06-14 01:53:17 +0100

    Updated

--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -10,21 +10,14 @@ const cacheAssets = [
 // Call Install Event
 self.addEventListener("install", (e) => {
   console.log("Service Worker: Installed");
-
-  e.waitUntil(
-    caches
-      .open(cacheName)
-      .then((cache) => {
-        console.log("Service Worker: Caching Files");
-        cache.addAll(cacheAssets);
-      })
-      .then(() => {
-        self.skipWaiting();
-      })
-  );
 });
 
 // Call Activate Events
 self.addEventListener("activate", (e) => {
   console.log("Service Worker: Activated");
+});
+
+// Fetch Event
+self.addEventListener("fetch", (evt) => {
+  console.log("fetch event", evt);
 });
