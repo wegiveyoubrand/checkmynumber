d67172a5e8269310788f6a26f527f182642fa1d9
wegiveyoubrand <wegiveyoubrand@gmail.com>
2021-09-03 10:52:31 +0100

    Updated

--- src/js/imports.js
+++ src/js/imports.js
@@ -1,4 +1,4 @@
-export const closeModalPopup = document.querySelectorAll(".close-modal"),
+const closeModalPopup = document.querySelectorAll(".close-modal"),
   Modal = document.querySelector(".modal"),
   menuToggle = document.querySelectorAll(".menu-toggle"),
   openModal = document.querySelectorAll(".open-modal"),
@@ -28,3 +28,44 @@ export const closeModalPopup = document.querySelectorAll(".close-modal"),
   GLO = ["0705", "0805", "0807", "0811", "0815", "0905"],
   AIRTEL = ["0701", "0708", "0802", "0808", "0812", "0902", "0907", "0901"],
   ETISALAT = ["0809", "0817", "0818", "0908", "0909"];
+
+function addeventListeners() {
+  // Slot Control event Listeners
+  slotControl.addEventListener("click", (e) => {
+    e.preventDefault;
+    all.slot.classList.toggle("active");
+  });
+
+  // Modal Control Event Listenerss
+  closeModalPopup.forEach((closeAction) => {
+    closeAction.addEventListener("click", () => {
+      Modal.classList.add("close");
+    });
+  });
+
+  openModal.forEach((openAction) => {
+    openAction.addEventListener("click", (e) => {
+      e.preventDefault;
+      Modal.classList.remove("close");
+      Modal.classList.add("active");
+    });
+  });
+
+  // Menu Control
+  menuToggle.forEach((toggle) => {
+    toggle.addEventListener("click", () => {
+      all.nav.classList.toggle("active");
+    });
+  });
+}
+
+function setUpSlotContainer() {
+  if (savedNumber.length < 1) {
+    slotContainer.innerHTML += `<div>No saved number</div>`;
+  } else {
+    slotContainer.innerHTML += slotContentLayout;
+  }
+}
+
+setUpSlotContainer();
+addeventListeners();
--- sw.js
+++ sw.js
@@ -1,44 +1,15 @@
 const cacheAssets = [
+  "/",
   "/index.html",
-  "/support.html",
-
   "/src/css/style.css",
-  "/src/css/font.css",
-  "/index.js",
-
+  "/src/js/setup.js",
+  "/src/js/imports.js",
   "/assets/images/logo-144px.png",
-
   "/assets/images/mtn.png",
   "/assets/images/glo.jfif",
   "/assets/images/airtel.png",
   "/assets/images/9mobile.png",
-  "/assets/images/Layer 1.png",
-
-  "/assets/src/fonts/poppins-v15-latin-100.eot",
-  "/assets/src/fonts/poppins-v15-latin-100.svg",
-  "/assets/src/fonts/poppins-v15-latin-100.ttf",
-  "/assets/src/fonts/poppins-v15-latin-100.woff",
-  "/assets/src/fonts/poppins-v15-latin-100.woff2",
-  "/assets/src/fonts/poppins-v15-latin-500.eot",
-  "/assets/src/fonts/poppins-v15-latin-500.svg",
-  "/assets/src/fonts/poppins-v15-latin-500.ttf",
-  "/assets/src/fonts/poppins-v15-latin-500.woff",
-  "/assets/src/fonts/poppins-v15-latin-500.woff2",
-  "/assets/src/fonts/poppins-v15-latin-700.eot",
-  "/assets/src/fonts/poppins-v15-latin-700.svg",
-  "/assets/src/fonts/poppins-v15-latin-700.ttf",
-  "/assets/src/fonts/poppins-v15-latin-700.woff",
-  "/assets/src/fonts/poppins-v15-latin-700.woff2",
-  "/assets/src/fonts/poppins-v15-latin-900.eot",
-  "/assets/src/fonts/poppins-v15-latin-900.svg",
-  "/assets/src/fonts/poppins-v15-latin-900.ttf",
-  "/assets/src/fonts/poppins-v15-latin-900.woff",
-  "/assets/src/fonts/poppins-v15-latin-900.woff2",
-  "/assets/src/fonts/poppins-v15-latin-regular.eot",
-  "/assets/src/fonts/poppins-v15-latin-regular.svg",
-  "/assets/src/fonts/poppins-v15-latin-regular.ttf",
-  "/assets/src/fonts/poppins-v15-latin-regular.woff",
-  "/assets/src/fonts/poppins-v15-latin-regular.woff2",
+  "/assets/images/Layer1.png",
 ];
 
 self.addEventListener("install", (e) => {
